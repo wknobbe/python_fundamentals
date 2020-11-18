7b641b8596cccbd77b06239ead83787c95d62e2f
wknobbe <wknobbe4@gmail.com>
2020-11-18 09:18:35 -0600

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+name = "Bill"
+print("Hello",name)
+print("Hello " + name)
+num = 4
+print("Hello",num)
+food1 = "pizza"
+food2 = "candy"
+print(f"I love {food1} and {food2}.")
