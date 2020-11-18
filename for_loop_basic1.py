714479060991a65f99efbc5fd0288492977511dd
wknobbe <wknobbe4@gmail.com>
2020-11-18 10:12:31 -0600

    Loop1

---
+++ for_loop_basic1.py
@@ -0,0 +1,29 @@
+# for x in range(0,151,1):
+#     print(x)
+
+# for x in range(5,1001,5):
+#     print(x)
+
+# for x in range(1,101,1):
+#     if x % 5 == 0:
+#         if x % 10 == 0:
+#             print("Coding Dojo")
+#         else:
+#             print("Coding")
+#     else:
+#         print(x)
+
+# sum = 0
+# for x in range(1,500001,2):
+#     sum += x
+# print(sum)
+
+# for x in range(2018,0,-4):
+#     print(x)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum,highNum+1,1):
+    if x % 3 == 0:
+        print(x)
